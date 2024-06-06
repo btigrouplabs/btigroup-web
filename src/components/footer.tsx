bb072727d4952e55a0c27f84017882535a3710da
Philip Dev <philip@btigroup.ee>
2024-06-06 12:01:49 +0200

    update layout - footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,92 +1,153 @@
-"use client";
-import type { NextPage } from "next";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { FaMailBulk } from "react-icons/fa";
+import {
+  FaGithub,
+  FaLinkedin,
+  FaMapLocation,
+  FaMedium,
+  FaTelegram,
+  FaXTwitter,
+} from "react-icons/fa6";
 
-const btiLogo = "/bti-logo-1@2x.png";
-const socialIcons = "/social-icons.svg";
-const socialIcons1 = "/social-icons-1.svg";
-
-const Footer: NextPage = () => {
-  const router = useRouter();
-
-  const onHomeContainerClick = () => {
-    router.push("/");
-  };
-
-  const onWalletContainerClick = () => {
-    router.push("/");
-  };
-
-  const onPrivacyPolicyContainerClick = () => {
-    router.push("/privacy-policy");
-  };
-  const onTermsContainerClick = () => {
-    router.push("/terms-conditions");
-  };
-
+export default function Footer() {
   return (
-    <footer className="self-stretch box-border overflow-hidden flex flex-col items-center justify-center py-[63px] px-5 max-w-full text-center text-9xl text-black-fonts-headings font-roboto-slab border-t-[2px] border-solid border-boder-g mq450:gap-[10px] mq450:pt-2.5 mq450:pb-2.5 mq450:box-border mq900:pt-5 mq900:pb-5 mq900:box-border">
-      <div className="w-[1496px] flex flex-row items-center justify-center gap-[20px] max-w-full mq450:gap-[20px_2px] mq900:self-stretch mq900:w-auto mq900:gap-[20px] mq1300:flex-wrap mq1300:justify-center mq1650:self-stretch mq1650:w-auto mq1650:flex-row mq1650:gap-[20px_120px]">
-        <Image
-          className="relative object-cover"
-          width={155}
-          height={132}
-          alt=""
-          src={btiLogo}
-        />
-        <div className="flex flex-row items-center justify-center p-[21px] box-border gap-[49px] max-w-full mq450:gap-[49px_2px] mq900:flex-wrap mq900:gap-[49px_20px]">
-          <div className="flex flex-row items-center justify-center">
-            <div
-              className="w-[79px] relative inline-block min-w-[79px] mq450:text-3xl cursor-pointer"
-              onClick={onHomeContainerClick}
-            >
-              Home
+    <footer className="self-stretch flex flex-row items-start justify-start pt-11 px-[25px] pb-[18px] box-border max-w-full text-left text-sm text-dominant font-roboto">
+      <div className="flex-1 flex flex-col items-start justify-start gap-[5.5px] max-w-full">
+        <div className="self-stretch flex flex-row items-start justify-center py-0 px-5 mq750:h-auto mq1050:h-auto">
+          <div className="flex-1 flex flex-row flex-wrap items-start justify-center py-0 px-0.5 box-border gap-[23px] max-w-[1200px] mq750:w-full mq750:h-auto mq1250:max-w-full">
+            <div className="self-stretch flex-1 flex flex-col items-center justify-center py-0 px-0 box-border gap-[18px] min-w-[120px] max-w-[180px]">
+              <Image
+                loading="lazy"
+                width={131}
+                height={131}
+                alt="BTI Group OÜ"
+                src="/img/logo.svg"
+              />
+
+              <div className="rounded-9xl flex flex-row items-center justify-center py-[13px] px-[30px] box-border gap-[11px] max-w-[195px]">
+                <FaXTwitter className="h-[18px] w-[18px] relative overflow-hidden shrink-0 min-h-[18px]" />
+                <FaLinkedin className="h-[18px] w-[18px] relative overflow-hidden shrink-0 min-h-[18px]" />
+                <FaTelegram className="h-[18px] w-[18px] relative overflow-hidden shrink-0 min-h-[18px]" />
+                <FaMedium className="h-[18px] w-[18px] relative overflow-hidden shrink-0 min-h-[18px]" />
+                <FaGithub className="h-[18px] w-[18px] relative overflow-hidden shrink-0 min-h-[18px]" />
+              </div>
             </div>
-          </div>
-          <div className="flex flex-row items-center justify-center cursor-pointer">
-            <div
-              className="w-[87px] relative inline-block min-w-[87px] mq450:text-3xl cursor-pointer"
-              onClick={onWalletContainerClick}
-            >
-              Wallet
+            <div className="flex-1 flex flex-col items-start justify-start gap-[16px] min-w-[120px] max-w-[180px]">
+              <b className="relative text-xl leading-[24px] font-semibold inline-block min-w-[70px] mq450:text-base mq450:leading-[19px]">
+                General
+              </b>
+              <div className="self-stretch relative leading-[14px]">
+                About BIT Group
+              </div>
+              <div className="self-stretch relative leading-[14px]">
+                BTI Communities
+              </div>
+              <div className="self-stretch relative leading-[14px]">Blog</div>
+              <div className="self-stretch relative leading-[14px]">
+                Announcements
+              </div>
+              <div
+                className="self-stretch relative leading-[14px] cursor-pointer"
+                // onClick={onCareersTextClick}
+              >
+                Careers
+              </div>
             </div>
-          </div>
-          <div
-            className="flex flex-row items-center justify-center cursor-pointer"
-            onClick={onPrivacyPolicyContainerClick}
-          >
-            <div className="w-[190px] relative inline-block mq450:text-3xl">
-              Privacy Policy
+            <div className="flex-1 flex flex-col items-start justify-start gap-[16px] min-w-[120px] max-w-[180px]">
+              <b className="relative text-xl leading-[24px] font-semibold inline-block min-w-[101px] mq450:text-base mq450:leading-[19px]">
+                Developers
+              </b>
+              <div className="self-stretch relative leading-[14px]">
+                Developer Hub
+              </div>
+              <div className="self-stretch relative leading-[14px]">
+                Tech Docs
+              </div>
+              <div className="self-stretch relative leading-[14px]">GitHub</div>
+              <div className="self-stretch relative leading-[14px]">Forum</div>
             </div>
-          </div>
-          <div
-            className="flex flex-row items-center justify-center cursor-pointer"
-            onClick={onTermsContainerClick}
-          >
-            <div className="w-[260px] relative inline-block mq450:text-3xl">{`Terms & Conditions`}</div>
+            <div className="flex-1 flex flex-col items-start justify-start gap-[16px] min-w-[120px] max-w-[180px]">
+              <a className="[text-decoration:none] relative text-xl leading-[24px] font-semibold text-[inherit] inline-block min-w-[78px] mq450:text-base mq450:leading-[19px]">
+                Services
+              </a>
+              <div className="self-stretch relative leading-[14px]">
+                BBA Chain
+              </div>
+              <div className="self-stretch relative leading-[14px]">
+                BTI Exchange
+              </div>
+              <div className="self-stretch relative leading-[14px]">
+                BTI Wallet
+              </div>
+              <div className="self-stretch relative leading-[14px]">
+                Quick Token Generator
+              </div>
+            </div>
+            <div className="flex-1 flex flex-col items-start justify-start gap-[16px] min-w-[120px] max-w-[180px]">
+              <b className="relative text-xl leading-[24px] font-semibold inline-block min-w-[95px] mq450:text-base mq450:leading-[19px]">
+                Resources
+              </b>
+              <div className="self-stretch relative leading-[14px]">
+                White Paper
+              </div>
+              <div className="self-stretch relative leading-[14px]">
+                BTI Echosystem
+              </div>
+              <div className="self-stretch relative leading-[14px]">
+                BTI Roadmap
+              </div>
+              <div className="self-stretch relative leading-[14px]">
+                BTI documents
+              </div>
+              <div className="self-stretch relative leading-[14px]">
+                BTI Presentation
+              </div>
+            </div>
+            <div className="flex-1 flex flex-col items-start justify-start gap-[18px] min-w-[120px] max-w-[180px] text-xl">
+              <b className="relative leading-[22px] font-bold inline-block min-w-[99px] mq450:text-base mq450:leading-[18px]">
+                Contact Us
+              </b>
+              <div className="self-stretch flex flex-col items-start justify-start py-0 px-0 gap-[9px] text-sm">
+                <div className="w-48 flex flex-row items-center justify-center gap-[8px]">
+                  <FaMailBulk className="h-[12.4px] w-4 relative" />
+                  <a
+                    className="flex-1 relative leading-[20px] text-[inherit] [text-decoration:none] whitespace-nowrap"
+                    href="mailto:contact@btigroup.ee"
+                    target="_blank"
+                  >
+                    contact@btigroup.ee
+                  </a>
+                </div>
+                <div className="self-stretch flex flex-row items-start justify-start gap-[8px]">
+                  <FaMapLocation className="h-[12.4px] w-4 relative" />
+                  <p className="m-0 flex-1 relative leading-[20px]">
+                    BTI Group OÜ, Tornimäe tn 3, Tallin, 10145, Estonia
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="h-[90px] w-[193px] flex flex-row items-center justify-center py-5 px-[13px] box-border gap-[67px] mq450:gap-[67px_2px]">
-          <Image
-            className="relative overflow-hidden shrink-0 min-h-[50px]"
-            loading="lazy"
-            height={50}
-            width={50}
-            alt=""
-            src={socialIcons}
-          />
-          <Image
-            className="relative overflow-hidden shrink-0 min-h-[50px]"
-            alt=""
-            height={50}
-            width={50}
-            src={socialIcons1}
-          />
+        <div className="self-stretch h-px flex flex-row items-start justify-start py-0 px-[74px] box-border max-w-full mq1250:pl-[37px] mq1250:pr-[37px] mq1250:box-border">
+          <div className="self-stretch flex-1 relative box-border max-w-full border-t-[1px] border-solid border-gray-100" />
+        </div>
+        <div className="self-stretch flex flex-row flex-wrap items-start justify-start max-w-full [row-gap:20px]">
+          <p className="m-0 flex-1 relative leading-[30px] font-light inline-block min-w-[452px] max-w-full mq750:min-w-full">
+            © 2024 BTI Group OÜ
+          </p>
+          <div className="flex-1 flex flex-row items-start justify-end gap-[12px] min-w-[452px] max-w-full text-center text-lime-200 mq750:min-w-full mq450:flex-wrap">
+            <div className="relative leading-[30px] font-light text-dominant inline-block min-w-[118px]">
+              All Rights Reserved
+            </div>
+            <u className="relative [text-decoration:underline] leading-[30px] font-light">
+              Terms and Conditions
+            </u>
+            <u className="relative [text-decoration:underline] leading-[30px] font-light inline-block min-w-[85px]">
+              Privacy Policy
+            </u>
+          </div>
         </div>
       </div>
     </footer>
   );
-};
-
-export default Footer;
+}
