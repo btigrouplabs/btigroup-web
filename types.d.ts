1f0fe72501193653f4492b159828f079d14ff00e
Philip Dev <philip@btigroup.ee>
2024-06-18 16:49:43 +0200

    implement authjs.dev

--- types.d.ts
+++ types.d.ts
@@ -1,24 +1,31 @@
-declare global {
-  interface IUser {
-    id: number;
-    uuid: string;
-    name: string;
-    email: string;
-  }
+// The `JWT` interface can be found in the `next-auth/jwt` submodule
+import { JWT } from "next-auth/jwt";
 
-  interface ISignInResponse {
-    access_token: string;
-  }
+declare module "next-auth" {
+  /**
+   * The shape of the user object returned in the OAuth providers' `profile` callback,
+   * or the second parameter of the `session` callback, when using a database.
+   */
+  interface User {}
 
-  interface ITokenPayload {
-    sub: number;
-    email: string;
-    username?: string;
-    isSecondFactorAuthenticated?: boolean;
-    iat?: number;
-    exp?: number;
+  /**
+   * The shape of the account object returned in the OAuth providers' `account` callback,
+   * Usually contains information about the provider being used, like OAuth tokens (`access_token`, etc).
+   */
+  interface Account {}
+
+  /**
+   * Returned by `useSession`, `auth`, contains information about the active session.
+   */
+  interface Session {
+    accessToken: string;
+    user: User;
   }
 }
 
-// This line ensures that the file is treated as a module
-export {};
+declare module "next-auth/jwt" {
+  /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
+  interface JWT {
+    accessToken: string;
+  }
+}
